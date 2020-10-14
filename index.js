5d1dd19e914bae43cd39a052cb81ebb40cf49b6a
ktagbo <kingsleytagbo@gmail.com>
2020-10-14 09:01:26 -0500

    Configuring Express

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .get('/', (req, res) => req.send('Hello World ...'))
+  .listen(port, () => console.log(`Listening on ${ port }`)
+);
+
+/*
 app.get('/', (request, response) => {
     response.send('Hello World ...');
 });
+*/
