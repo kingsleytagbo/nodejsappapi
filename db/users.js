98b457cfae0ea955b5967b403b46dbe396123cb4
ktagbo <kingsleytagbo@gmail.com>
2020-10-28 18:31:21 -0500

    /users/createUser

--- db/users.js
+++ db/users.js
@@ -43,5 +43,49 @@ const loginUser = (request, response, next) => {
     }
 }
 
+const createUser = (request, response, next) => {
+    response.setHeader('Content-Type', 'application/json');
+    try {
+        const login = (request && request.body) ? JSON.parse(request.body).login : {};
+        const user = (request && request.body) ? JSON.parse(request.body).user : {};
+        const username = ((login && login.username) || '').trim().toLowerCase();
+        const password = ((login && login.password) || '');
+        const guest = { user_login: username, user_pass: password };
+        POOLS.pool.query('SELECT * FROM wp_user WHERE user_login = $1 and user_pass = $2', [guest.user_login, guest.user_pass], (error, data) => {
+            if (error) {
+                throw error
+            }
 
-module.exports = { getUsers, loginUser };
+            const result = (data && data.rows && data.rows.length > 0) ?
+                { authenticated: true, auth_token: (new Date()).toISOString() } :
+                { authenticated: false, auth_token: null };
+            if (result.authenticated === true) {
+                const [user_login, user_pass, user_nicename,user_email,display_name,
+                    user_status,user_registered,user_url,user_activation_key,spam,
+                    deleted,site_id] = user;
+                POOLS.pool.query('INSERT INTO wp_user (user_login, user_pass, user_nicename,user_email,display_name,user_status,user_registered,user_url,user_activation_key,spam, deleted,site_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)', 
+                [user_login, user_pass, user_nicename,user_email,display_name,
+                    user_status,user_registered,user_url,user_activation_key,spam,
+                    deleted,site_id], (error, results) => {
+                    if (error) {
+                        throw error
+                    }
+                    response.status(201).send(`createUserSuccess: ${result.insertId}`)
+                });
+            }
+            else {
+                console.log({ cerateUserFailure: guest, body: request.body, user: user });
+                response.status(200).send(result);
+            }
+        });
+        // next();
+    }
+    catch (error) {
+        console.log({ saveUserApiFailure: error });
+        response.status(500).send({});
+        next();
+    }
+}
+
+
+module.exports = { getUsers, loginUser, createUser };
