8005bcf45beab0b1d557ef6d6b3f04ccbabb9dbd
ktagbo <kingsleytagbo@gmail.com>
2020-10-28 14:05:24 -0500

    heroku + postgress pg

--- db/users.js
+++ db/users.js
@@ -15,28 +15,29 @@ const getUsers = (request, response) => {
     response.setHeader('Content-Type', 'application/json');
     try {
         const login = (request && request.body && request.body.login) ? JSON.parse(request.body).login : {};
-        const username = ((login && login.username) || '').trim().toLowerCase();
-        const password = ((login && login.password) || '');
-        const user = {user_login: username, user_pass: password};
-        POOLS.pool.query('SELECT * FROM wp_user WHERE user_login = $1 and user_pass = $2', [user.user_login, user.user_pass], (error, data) => {
+        const username = ((login && login.username) || 'adminuser').trim().toLowerCase();
+        const password = ((login && login.password) || 'adminpassword1');
+        const guest = {user_login: username, user_pass: password};
+        POOLS.pool.query('SELECT * FROM wp_user WHERE user_login = $1 and user_pass = $2', [guest.user_login, guest.user_pass], (error, data) => {
             if (error) {
-                throw error
+              throw error
             }
-            const result = (data && data.rows && data.rows.length > 0) ?
-                { authenticated: true, auth_token: (new Date()).toISOString() } :
-                { authenticated: false, auth_token: null };
-            if (result.authenticated === true) {
+            console.log({loginUser: data.rows, guest: guest});
+
+            const result = (data && data.rows && data.rows.length > 0)  ? 
+            {authenticated:true, auth_token: (new Date()).toISOString()} : 
+            {authenticated:false, auth_token: null};
+            if(result.authenticated === true){
                 response.status(200).send(result);
             }
-            else {
-                console.log({loginFailure: result, user: user});
+            else{
                 response.status(200).send(result);
             }
         });
         next();
     }
     catch (error) {
-        console.log({loginApiFailur: error});
+        console.log(error);
         response.status(500).send({});
         next();
     }
