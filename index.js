e97fb77cc6961e3f4ba68618ed8adf233e5c88ce
ktagbo <kingsleytagbo@gmail.com>
2020-10-28 12:21:03 -0500

    heroku + postgress pg

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const app = express();
 const pool = require('./db/postgress/connection');
 const Users = require('./db/users');
 
-
-
 //Here we are configuring express to use body-parser as middle-ware.
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
