23f23d462d093020aba2b8c9ce702290958fb902
ktagbo <kingsleytagbo@gmail.com>
2020-10-29 10:39:47 -0500

    /users/getUsers

--- db/users.js
+++ db/users.js
@@ -7,7 +7,7 @@ const getUsers = (request, response) => {
         if (error) {
             throw error
         }
-        response.status(200).json(results.rows)
+        response.status(200).send(results.rows)
     })
 };
 
