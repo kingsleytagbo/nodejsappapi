f23071b4fe6bc94f8dbf8d92b37fbff64967fdd2
ktagbo <kingsleytagbo@gmail.com>
2020-11-10 13:17:33 -0600

    options

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     if (req.methods === 'OPTIONS'){
+        cosole.log({options: req});
         res.sendStatus(204)
     }
     next();
