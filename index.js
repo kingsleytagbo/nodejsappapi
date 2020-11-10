211612fd826c7eed4387e1debbbb1680842d160a
ktagbo <kingsleytagbo@gmail.com>
2020-11-10 13:14:38 -0600

    options

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    if (req.methods === 'OPTIONS'){
+        res.sendStatus(204)
+    }
     next();
 });
 
